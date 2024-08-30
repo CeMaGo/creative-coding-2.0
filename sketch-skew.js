283aa21c8259995a771309df7fd7ae425e36df1c
CeMaGo <119850565+CeMaGo@users.noreply.github.com>
2024-08-30 10:22:55 +0200

    save befor refactor

--- sketch-skew.js
+++ sketch-skew.js
@@ -3,34 +3,42 @@ const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  // animate: true,
 };
 
-const sketch = () => {
+const sketch = ({context, width, height }) => {
   let x, y, w, h;
   let angle, rx, ry;
 
   const num = 20;
+  const degrees = 30;
 
+  const rects = [];
+
+  for (let i = 0; i < num; i++){
+    let x = random.range( 0, width);
+    let y = random.range(0, height);
+    let w = random.range(200, 600);
+    let h = random.range(40, 200);
+
+    rects.push({x, y, w, h })
+  }
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    for (let i = 0; i < num; i++){
+  rects.forEach(rect => {
+    context.save();
+    context.translate( rect.x, rect.y ); 
+    context.strokeStyle = 'blue';  
+    
+    drawSkewRect({context, w, h, degrees});
+    context.stroke( );
 
-      let x = random.range( 0, width);
-      let y = random.range(0, height);
-      let w = width * 0.6;
-      let h = height * 0.1;
-  
-      context.save();
-      context.translate( x, y ); 
-      context.strokeStyle = 'blue';  
-      
-      drawSkewRect({context});
-      context.stroke( )
-      context.restore();
-    }
+    context.restore();
+
+  })
     
   };
 };
