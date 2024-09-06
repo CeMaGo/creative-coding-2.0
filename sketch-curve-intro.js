0e537076a473875f8682907c96b654aab6893011
CeMaGo <119850565+CeMaGo@users.noreply.github.com>
2024-09-06 12:25:27 +0200

    starting new sketch learning about curves

---
+++ sketch-curve-intro.js
@@ -0,0 +1,19 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.beginPath();
+    context.moveTo(200,540);
+    context.lineTo(800,540);
+    context.stroke();
+  };
+};
+
+canvasSketch(sketch, settings);
